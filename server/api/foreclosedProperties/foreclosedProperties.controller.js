03b9948d585f51cc495597c5a8ccadf43264d22d
Faisal Nawaz <fslnwz0@gmail.com>
2016-07-17 21:15:09 -0400

    working scrape of brooklyn foreclosures;

--- server/api/foreclosedProperties/foreclosedProperties.controller.js
+++ server/api/foreclosedProperties/foreclosedProperties.controller.js
@@ -4,30 +4,34 @@
  */
 
 'use strict';
-var $ = require('cheerio')
-var request = require('request')
+var $ = require('cheerio');
+var request = require('request');
+var stringify = require('json-stringify-safe');
 
 
 // Gets a list of ForeclosedPropertiess
-exports.index = function(req, res) {
+exports.index = function (req, res) {
   var pdfArray = []
+
   function gotHTML(err, resp, html) {
     if (err) return console.error(err)
     var parsedHTML = $.load(html)
-    // get all pdf tags and loop over them
-    parsedHTML('a').map(function(i, link) {
+      // get all pdf tags and loop over them
+    parsedHTML('a').map(function (i, link) {
       var href = $(link).attr('href');
       if (href) {
         if (!href.match('.pdf')) return
-        pdfArray.push({'link': domain + href,
-      "name": $(link).contents()})
+        pdfArray.push({
+          'PDFlink': domain + '/courts/2jd/kings/Civil/' + href,
+          'address': href.split('/')[2].replace(/-/g, ' '),
+          'week': href.split('/')[1],
+        })
       }
     });
-    console.log(pdfArray, 'link');
 
+    res.json(pdfArray);
   }
 
   var domain = 'http://www.nycourts.gov/courts/2jd/kings/civil/foreclosuresales.shtml'
   request(domain, gotHTML)
-  res.json(pdfArray);
 };
